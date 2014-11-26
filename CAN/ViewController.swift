3ce5323ff74c0ecec85537b1ba4d7193c021cb16
Justin <dadlipsticks@local-4.attlocal.net>
2014-11-25 21:23:24 -0600

    Initial Commit

---
+++ CAN/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CAN
+//
+//  Created by Dad on 11/25/14.
+//  Copyright (c) 2014 Corporate Angel Network. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
